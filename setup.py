5e7b626ebff1380286ea962e1add825523a5253c
Shenggao <zshgao@gmail.com>
2016-05-18 12:46:10 +0800

    correct license in setup.py to MIT

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
